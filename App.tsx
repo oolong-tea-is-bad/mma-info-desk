72a41d498cf2e5f461b8a1334334930959225f55
Juno Lee <jl12818@nyu.edu>
2024-06-24 16:55:20 +0900

    Second Commit

--- App.tsx
+++ App.tsx
@@ -1,22 +1,51 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet, Button, Alert, Pressable } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={[styles.safeAreaView]}>
-      <Text style={[styles.textWelcome]}>병무청 본관에 오신걸 환영합니다!{'\n'} 
+      <Text style={[styles.welcomeText]}>병무청 본관에 오신걸 환영합니다!{'\n'} 
         무엇을 도와드릴까요?
       </Text>
       <View style={[styles.view]}>
-        <View style={[]}>
-          <View style={[styles.box]}></View>
-          <View style={[styles.box]}></View>
-          <View style={[styles.box]}></View>
+        <View style={[styles.content]}>
+          <Pressable 
+            style={[styles.box]} 
+            onPress={() => Alert.alert("신체검사는 민원봉사실 2층을 이용해주십시오.")}
+            >
+            <Text style={[styles.boxText]}>신체검사</Text>
+          </Pressable>
+          <Pressable 
+            style={[styles.box]} 
+            onPress={() => Alert.alert("병역명문가 관련 서류 발급은 민원봉사실 1층을 이용해주십시오.")}
+            >
+            <Text style={[styles.boxText]}>병역명문가</Text>
+          </Pressable>
+          <Pressable 
+            style={[styles.box]} 
+            >
+            <Text style={[styles.boxText]}>예비군</Text>
+          </Pressable>
         </View>
-        <View>
-          <View style={[styles.box]}></View>
-          <View style={[styles.box]}></View>
-          <View style={[styles.box]}></View>
+        <View style={[styles.content]}>
+          <Pressable 
+            style={[styles.box]} 
+            onPress={() => Alert.alert("병역명문가 관련 서류 발급은 민원봉사실을 이용해주십시오.")}
+            >
+            <Text style={[styles.boxText]}>사회복무요원</Text>
+          </Pressable>
+          <Pressable 
+            style={[styles.box]} 
+            onPress={() => Alert.alert("병역명문가 관련 서류 발급은 민원봉사실을 이용해주십시오.")}
+            >
+            <Text style={[styles.boxText]}>생계 감면</Text>
+          </Pressable>
+          <Pressable 
+            style={[styles.box]} 
+            onPress={() => Alert.alert("병역명문가 관련 서류 발급은 민원봉사실을 이용해주십시오.")}
+            >
+            <Text style={[styles.boxText]}>기타 업무</Text>
+          </Pressable>
         </View>
         
       </View>
@@ -26,7 +55,11 @@ export default function App() {
 // prettier-ignore
 const styles = StyleSheet.create({
   safeAreaView: {flex: 1, padding: 10,},
-  view: {height: '100%', borderWidth: 1, },
-  textWelcome: {fontWeight: "bold", fontSize: 30, textAlign: 'center', borderWidth: 1, marginTop: 10},
-  box: {width: 300, height: 80, backgroundColor: 'orange', borderWidth: 1},
+  view: {height: '100%', borderWidth: 1, flex: 2, justifyContent: 'space-evenly'},
+  welcomeText: {fontWeight: "bold", fontSize: 30, textAlign: 'center', borderWidth: 1, marginTop: 10},
+  content: {flexDirection: 'row', justifyContent: 'space-evenly', borderWidth: 1},
+  box: {width: 300, height: 300, backgroundColor: 'orange', borderWidth: 1, justifyContent: 'center',
+    alignItems: 'center', borderRadius: 4
+  },
+  boxText: {fontSize: 30}
 })
